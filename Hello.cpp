9d378ec21f37f673ad97a857c37a7b9a7a57ab82
Isaac R <ruizisaac278@gmail.com>
2024-02-15 14:43:54 -0800

    Introduction to Github

---
+++ Hello.cpp
@@ -0,0 +1,11 @@
+/***
+ * {Lab 1 - a short introduction to Github and how it works}
+***/
+
+using namespace std;
+
+int main () {
+    cout <<"Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0
+}
