498970823211e131c30435dfe67a5683ecefa807
Isaac R <ruizisaac278@gmail.com>
2024-03-07 18:19:13 -0800

    Checking something

--- Hello.cpp
+++ Hello.cpp
@@ -2,10 +2,12 @@
  * {Lab 1 - a short introduction to Github and how it works}
 ***/
 
+#include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main () {
     cout <<"Hello World!" << endl;
     printf("Hello World again!\n");
-    return 0
+    return 0;
 }
